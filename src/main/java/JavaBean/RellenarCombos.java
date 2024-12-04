5ff79bd307f516a3a8b9074ae6988796d451c1ff
AxeelJose <axeeljose@gmail.com>
2024-12-04 10:40:56 -0500

    Código para Administrador
    Código completo para el Administrador, ajustes en botones, cambios mínimos del frontend

--- src/main/java/JavaBean/RellenarCombos.java
+++ src/main/java/JavaBean/RellenarCombos.java
@@ -17,7 +17,7 @@ public class RellenarCombos {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String SUrl, SUser, SPass;
-            SUrl = "jdbc:mysql://bfsytg3rbkdzoanfa8sa-mysql.services.clever-cloud.com:3306/bfsytg3rbkdzoanfa8sa";
+            SUrl = "jdbc:mysql://ulyfwrg1vfpqzj0x:84YlnLU1hjjP2sJdHtn7@hv-par8-022.clvrcld.net:13867/b7n2pybqwwnmmvd4p8wd";
             SUser = "ulyfwrg1vfpqzj0x";
             SPass = "84YlnLU1hjjP2sJdHtn7";
 
@@ -58,4 +58,56 @@ public class RellenarCombos {
             }
         }
     }
+
+    public void RellenarComboBoxMeseros(String tabla, String valor, JComboBox combo, String rol) {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String SUrl, SUser, SPass;
+            SUrl = "jdbc:mysql://ulyfwrg1vfpqzj0x:84YlnLU1hjjP2sJdHtn7@hv-par8-022.clvrcld.net:13867/b7n2pybqwwnmmvd4p8wd";
+            SUser = "ulyfwrg1vfpqzj0x";
+            SPass = "84YlnLU1hjjP2sJdHtn7";
+
+            String sql;
+            if (tabla.equals("usuario")) {
+                // Modificar la consulta para filtrar por rol
+                sql = "SELECT CONCAT(apellidos, ', ', nombres) AS nombre_completo FROM " + tabla + " WHERE rol = '" + rol + "'";
+            } else {
+                sql = "SELECT * FROM " + tabla;
+            }
+
+            con = DriverManager.getConnection(SUrl, SUser, SPass);
+            st = con.createStatement();
+            rs = st.executeQuery(sql);
+
+            combo.removeAllItems();
+            while (rs.next()) {
+                if (tabla.equals("usuario")) {
+                    combo.addItem(rs.getString("nombre_completo"));
+                } else {
+                    combo.addItem(rs.getString(valor));
+                }
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
 }
